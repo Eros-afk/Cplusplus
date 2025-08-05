f0414f62598fd570594fb1be7845d51a1308342a
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-05 14:01:50 -0300

    Iniciando o c plus plus

---
+++ Atividade1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    // std::cout << "Hello, World!" << std::endl;
+
+    // std::cout << "Vou\n" 
+    //           << "Fazer\n"
+    //           << "Um\n"
+    //           << "Programa\n"
+    //           << "Em\n"
+    //           << "C++\n";
+
+    //Created by Eros-afk
+    /*The maioral
+    The chosen one
+    The best programmer in the world
+    The legend
+    The myth
+    */
+
+    std::cout << "Vou\n" 
+                << "Fazer\n"
+                << "Um\n"
+                << "Programa\n"
+                << "Em\n"
+                << "C++\n";
+    
+
+    return 0;
+}
---
+++ Primeiro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//Diretiva de pré-processamento para incluir a biblioteca de entrada e saída padrão
+// Esta biblioteca é necessária para usar std::cout e std::endl
+
+//using namespace std;
+
+int main() {
+    //cout comando de saida de dados
+    //std::endl é usado para inserir uma nova linha (\n também serve) e limpar o buffer de saída
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
