dc2fa54175447109ac94537d194e5b64e07fcf61
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-12 11:21:38 -0300

    #

--- Atividade2.cpp
+++ Atividade2.cpp
@@ -4,10 +4,10 @@ int main() {
     int NumComputadores = 100;
     int SerialComputadores = 11111;
 
-    std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
+    std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
     std::cout << "Numero de computadores: " << NumComputadores << std::endl;
     std::cout << "Numero de serie dos computadores: " << SerialComputadores << std::endl;
-    std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
+    std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;
     
     return 0;
 }
