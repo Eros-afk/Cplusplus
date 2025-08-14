26010dfc177ed3a472348c9114e3ae255d4e481e
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-14 15:31:35 -0300

    #

--- Constantes.cpp
+++ Constantes.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 //Usar define para criar constantes
-#define MAX 100 //Define é uma macro, não é uma variável, não ocupa espaço na memória, é substituído pelo pré-processador
+//#define MAX 100 //Define é uma macro, não é uma variável, não ocupa espaço na memória, é substituído pelo pré-processador
 
 int main(){
-    //const int MAX = 100; LOCAL NA MEMORIA
+    const int MAX = 100;
+    //const é uma constante, ocupa espaço na memória, é uma variável de leitura apenas 
     int TotalMAX;
     TotalMAX = MAX + 1;
     std::cout << "O valor máximo é: " << TotalMAX << std::endl;
     std::cout << "O valor de MAX é: " << MAX << std::endl;
+    std::cout << "O endereço de MAX é: " << &MAX << std::endl;
     return 0;
 }
