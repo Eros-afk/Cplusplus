41afa1edbc026083beb3d27bd214a5e612813ade
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-14 16:02:44 -0300

    Valores

---
+++ Seção5/RecebendoValores.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int Numero1, Numero2, Soma;
+    std::cout << "Digite o primeiro numero: ";
+    std::cin >> Numero1;  
+    std::cout << "Digite o segundo numero: ";
+    std::cin >> Numero2;  
+}
