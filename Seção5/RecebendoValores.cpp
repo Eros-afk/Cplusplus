b887e27c0912d4cee4e2a933d3d87c4d346f87d1
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-14 16:08:30 -0300

    #

--- Seção5/RecebendoValores.cpp
+++ Seção5/RecebendoValores.cpp
@@ -6,4 +6,11 @@ int main(){
     std::cin >> Numero1;  
     std::cout << "Digite o segundo numero: ";
     std::cin >> Numero2;  
+    Soma = Numero1 + Numero2;
+    std::cout << "A soma dos numeros e: " << Soma << std::endl;
+    std::cout << "A subtração dos numeros e: " << Numero1 - Numero2 << std::endl;
+    std::cout << "A multiplicação dos numeros e: " << Numero1 * Numero2 << std::endl;
+    std::cout << "A divisão dos numeros e: " << Numero1 / Numero2 << std::endl;
+    std::cout << "O resto da divisão dos numeros e: " << Numero1 % Numero2 << std::endl;
+    return 0;
 }
