91252aa4473e28eaf109903718117d5ea1f3aaa6
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-15 16:35:22 -0300

    Atividade3

---
+++ Seção5/Atividade3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    float Numero1, Numero2, Numero3, Media;
+    std::cout << "Digite o primeiro numero: ";
+    std::cin >> Numero1;
+    std::cout << "Digite o segundo numero: ";
+    std::cin >> Numero2;
+    std::cout << "Digite o terceiro numero: ";
+    std::cin >> Numero3;
+    Media = (Numero1 + Numero2 + Numero3) / 3;
+    std::cout << "A media dos numeros e: " << Media << std::endl;
+} 
