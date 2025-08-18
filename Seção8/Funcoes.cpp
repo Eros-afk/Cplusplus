f2fe15151fa75745748644ff1b45c23b9fa1f939
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-18 11:14:48 -0300

    func

--- Seção8/Funcoes.cpp
+++ Seção8/Funcoes.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
+//O que a função vai retornar
+//Tipo de retorno da função 
+int soma(int num, int numsomado){
+    /*
+    //Variável local
+    int resultado = num + numsomado;
+    return resultado; //Retorna o resultado da soma
+    */
+
+    return num + numsomado; //Retorna a soma diretamente
+    //A função soma recebe dois parâmetros, num e numsomado, e retorna a soma deles.
+}
+
 int main(){
-    int num;
+    int num, numsomado;
     std::cout << "Digite um número: ";
     std::cin >> num;
-    std::cout << "A soma deste numero com 100: " << num + 100 << std::endl;
-    std::cout << "A soma deste numero com 1000: " << num + 1000 << std::endl;
+    std::cout << "Digite o numero a ser somado: ";
+    std::cin >> numsomado;
+    std::cout << "A soma deste numero: " << num << " + " << numsomado << " e: " << soma(num, numsomado) << std::endl;
     return 0;
 }
