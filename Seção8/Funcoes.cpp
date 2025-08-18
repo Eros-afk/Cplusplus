a9469bea0fba79385610437f30c3fe16c1c438e1
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-18 13:41:19 -0300

    #

--- Seção8/Funcoes.cpp
+++ Seção8/Funcoes.cpp
@@ -13,8 +13,17 @@ int soma(int num, int numsomado){
     //A função soma recebe dois parâmetros, num e numsomado, e retorna a soma deles.
 }
 
+void exibirMensagem(){
+    std::cout << "Esta é uma função que não retorna nada." << std::endl;
+    //Função void não retorna nada, apenas executa uma ação
+    //Pode ser usada para exibir mensagens ou realizar ações sem necessidade de retorno
+    std::cout << "Função executada com sucesso!" << std::endl;
+    return; //Opcional, pois ao final de uma função void já é implícito o retorno
+}
+
 int main(){
     int num, numsomado;
+    exibirMensagem(); //Chama a função que exibe uma mensagem
     std::cout << "Digite um número: ";
     std::cin >> num;
     std::cout << "Digite o numero a ser somado: ";
