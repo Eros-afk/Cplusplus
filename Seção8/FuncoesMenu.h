a5e3eb645454b373c42cb36f716d2095310e59c6
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-19 09:19:37 -0300

    #

---
+++ Seção8/FuncoesMenu.h
@@ -0,0 +1,5 @@
+//protótipos das funções
+// protótipos são declarações de funções que informam ao compilador que a função existe, mas não fornecem a implementação completa. Eles são usados para permitir que o compilador reconheça as funções antes de serem definidas.
+void ExibirMenu();
+int RetornarEscolha();
+int ProcessarEscolha(int TipoEscolha);
