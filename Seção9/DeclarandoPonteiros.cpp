afcec138463c5b33ee2c3aacbd5cb51865c7627a
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-20 07:49:53 -0300

    Ponteiros

---
+++ Seção9/DeclarandoPonteiros.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int *ptr; // Declaração de um ponteiro nulo
+    int num = 101;
+    ptr = &num; // Atribuição do endereço da variável num ao ponteiro ptr
+    *ptr = 202; // Modifica o valor da variável num através do ponteiro ptr
+
+    std::cout << "Valor da variável num: " << num << std::endl; // Exibe o valor modificado
+    std::cout << "Valor do ponteiro ptr: " << *ptr << std::endl; // Exibe o valor apontado pelo ponteiro
+    std::cout << "Endereço da variável num: " << &num << std::endl;
+    std::cout << "Endereço do ponteiro ptr: " << &ptr << std::endl;
+    std::cout << "Endereço armazenado no ponteiro ptr: " << ptr << std::endl; // Exibe o endereço armazenado no ponteiro
+
+    return 0;
+}
