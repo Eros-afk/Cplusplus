32e7cd297ea4d4993de8e56c69fbed54d9a54485
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-21 15:37:13 -0300

    #

--- Seção11/ArraysVetores.cpp
+++ Seção11/ArraysVetores.cpp
@@ -3,22 +3,19 @@
 #include <iomanip>
 
 int main(){
-    float nota1, nota2, nota3, nota4, nota5, media;
+    //Devem ter todos os compartimentos o mesmo tipo de dados e por isso ela é considerada uma variável composta (varios locais de armazenamento) e homogênea (todos os locais de armazenamento são do mesmo tipo).
+    float nota[5];
+    float media, soma{0.0f};
     std::string NomeDoAluno;
 
     std::cout << "Digite o nome do aluno: ";
     std::cin >> NomeDoAluno;
-    std::cout << "Digite a nota 1: ";
-    std::cin >> nota1;
-    std::cout << "Digite a nota 2: ";
-    std::cin >> nota2;
-    std::cout << "Digite a nota 3: ";
-    std::cin >> nota3;
-    std::cout << "Digite a nota 4: ";
-    std::cin >> nota4;
-    std::cout << "Digite a nota 5: ";
-    std::cin >> nota5;
+    for(int i = 0; i < 5; i++) {
+        std::cout << "Digite a nota " << (i + 1) << ": ";
+        std::cin >> nota[i];
+        soma += nota[i];
+    }
 
-    media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+    media = soma / 5;
     std::cout << "A media do aluno " << NomeDoAluno << " e: " << std::fixed << std::setprecision(9) << media << std::endl;
 }
