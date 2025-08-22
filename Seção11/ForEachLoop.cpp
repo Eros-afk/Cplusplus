ab570da1f02530501e452fa497923458a3fabc6b
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-22 13:33:23 -0300

    foreach

---
+++ Seção11/ForEachLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    //int anos[] {1998, 2011, 2014, 2017, 2020};
+    for (auto ano : {1998, 2011, 2014, 2017, 2020})
+    {
+        std::cout << ano << std::endl;
+    }
+     
+    return 0;
+}
