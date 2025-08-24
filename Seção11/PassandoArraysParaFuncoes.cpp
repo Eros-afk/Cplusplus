9f4c082f9c630ebacc05fecdb0caf04172908e95
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-24 16:40:53 -0300

    Colocando Array como ptr

--- Seção11/PassandoArraysParaFuncoes.cpp
+++ Seção11/PassandoArraysParaFuncoes.cpp
@@ -2,6 +2,7 @@
 #include <array>
 
 void mostrarVetor(int Array[], int tamanhoVetor);
+void alterarVetor(int Array[], int tamanhoVetor);
 
 int main(){
     int vetor[] = {1,2,3,4,5,6,7,8,9,10};
@@ -10,20 +11,37 @@ int main(){
     //Pois são gastos 4bytes por int neste vetor e como são 10 elementos o vetor terá tamanho 40 bytes
     //Mas preciso do tamanho
     //Basta dividir pelo tipo do vetor
-
     int tamanhoVetor = std::size(vetor);
-
     //int tamnhoVetor = sizeof(vetor) / sizeof(int);
     //Agora serão 40 bytes / 4 bytes = 10 e teremos o tamanho do vetor!
     std::cout << "Tamanho do Vetor: " << tamanhoVetor << std::endl;
     std::cout << "Tamanho de um float " << sizeof(float) << std::endl;
     std::cout << "Tamanho de um int " << sizeof(int) << std::endl;
 
+    std::cout << "Vetor Original" << std::endl;
+    //Aqui seria o mesmo que &vetor[0]
     mostrarVetor(vetor, tamanhoVetor);
+    alterarVetor(vetor, tamanhoVetor);
+    std::cout << "Vetor Alterado?" << std::endl;
+    mostrarVetor(vetor, tamanhoVetor);
+    /*O nome de um vetor em C e C++ é o mesmo que &Vetor[0]
+    O vetor foi alterado pois foi passado por referencia
+    */
+
+    return 0;
 }
 
-void mostrarVetor(int Array[], int tamanhoVetor){
+/*Então int Array[] indica que isso na realidade é um ponteiro
+Podemos substituir por*/
+void mostrarVetor(int *Ptr_Array, int tamanhoVetor){
     for (int i = 0; i < tamanhoVetor; i++){
-        std::cout << "|" << Array[i] << "|";
+        std::cout << "|" << Ptr_Array[i] << "|";
+    }
+    std::cout << "\n\n";
+}
+
+void alterarVetor(int *Ptr_Array, int tamanhoVetor){
+    for(int i = 0; i < tamanhoVetor; i++){
+        Ptr_Array[i] = Ptr_Array[i]*2;
     }
 }
