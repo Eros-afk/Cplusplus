38a73391ffdc99f7483d95854bd0082affe41a01
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-24 21:59:17 -0300

    Classe1.1

--- Seção13/Classe.cpp
+++ Seção13/Classe.cpp
@@ -4,25 +4,39 @@ class Casa{
     /*Estes são os atributos, as propriedades da classe e são representados por variáveis
     Serão as características de cada objeto criado a partir desta classe
     Quando o programa estiver sendo executado e queremos saber qual é a situação do objeto em relação ao valor de seus atributos dizemos que queremos saber o ESTADO do objeto*/
-    int numQuartos;
-    float tamanho;
-    //No geral coloca b na frente de uma variável bool para identificar que ela é do tipo booleana(bool)
-    bool bTemSuite;
-    //Estas são as operações da classe, ou métodos ou as funções que o objeto poderá executar quando for criado(instanciado) a partir desta classe.
-    void mostrarTamanho();
-    int obtenhaNumeroDeQuartos();
-    bool temSuite();
+    //Por padrão em uma classe a visibilidade é do tipo private
+    private:
+    //Private: esta indicando que estes atributos só poderão ser acessados dietamente no código da classe Casa
+        int numQuartos{4};
+        float tamanho{90.0f};
+        //No geral coloca b na frente de uma variável bool para identificar que ela é do tipo booleana(bool)
+        bool bTemSuite{true};
+        /*Public significa que estará acesível fora do código da classe*/
+    public:
+        //Estas são as operações da classe, ou métodos ou as funções que o objeto poderá executar quando for criado(instanciado) a partir desta classe.
+        void mostrarTamanho();
+        int obtenhaNumeroDeQuartos();
+        bool temSuite();
 };
 
 int main(){
     /*Quando declaramos variáveis de tipos int, char, float, double, etc poderemos agora declarar variaveis tendo como tipo uma classe
     Se for do tipo classe, um local na memória será reservado para armazenar essa variável*/
     Casa CasaDePraia;
+    //Como a função main está fora do código da classe, dentro dela não será possível acessar membros privados da classe Casa!
+    //Contudo poderá ser acessado os membros public
+    CasaDePraia;
+    CasaDePraia.mostrarTamanho();
+    std::cout << "\n Num Quartos: " << CasaDePraia.obtenhaNumeroDeQuartos();
+    std::cout << "\n Tem Suites: " << CasaDePraia.temSuite() ? std::cout << "Sim" : std::cout << "Não" << std::endl;
+    /*Para acessar os membros da classe Casa usamos neste caso a notação ponto
+    NomeDoObjeto.NomeDoAtributo ou NomeDoObjeto.NomeDoMetodo ou função*/
 
     return 0;
 }
 
-/*O operador escopo indica que o método pertence a classe casa*/
+/*O operador escopo indica que o método pertence a classe casa
+Estes códigos pertencem a classe casa e portanto podem acessar diretamente os seus atributos privados*/
 void Casa::mostrarTamanho()
 {
     std::cout << "\nTamanho: " << tamanho;
