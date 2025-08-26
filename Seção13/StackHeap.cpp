31a6440a1d67851e3ca8146edd9cc45f06b0534c
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-26 08:16:49 -0300

    StackHeap correto

--- Seção13/StackHeap.cpp
+++ Seção13/StackHeap.cpp
@@ -11,7 +11,7 @@ int varGlobal {10};
 int main(){
     /*Como main() é uma função a variavel varMain é local a função main(), portanto será armazenada na região de memória destinnada a variáveis locais e chamadas de funções (STACK - PILHA)*/
     int varmain{0};
-    funcao1;
+    funcao1();
     return 0;
 }
 
@@ -22,10 +22,10 @@ void funcao1()
     /*Variável Estática, será armazenada na região de memória da RAM destinada a Variáveis Globais e Variáveis Estáticas*/
     static int varEstatica{20};
     std::cout << "Chamando Funcao01\n";
-    std::cout << "Endereco var01" << &var01;
+    std::cout << "Endereco var01: " << &var01;
     varEstatica++;
     std::cout << "\nValor varEstatica: " << varEstatica << std::endl;
-    funcao2;
+    funcao2();
 }
 
 void funcao2()
@@ -33,8 +33,8 @@ void funcao2()
     /*armazenada na região de memória destinnada a variáveis locais e chamadas de funções (STACK - PILHA)*/
     int var02 { 2 };
     std::cout << "Chamando Funcao02\n";
-    std::cout << "Endereco var02" << &var02;
-    funcao3;
+    std::cout << "Endereco var02: " << &var02;
+    funcao3();
     
 }
 
@@ -42,15 +42,15 @@ void funcao3()
 {
     /*armazenada na região de memória destinnada a variáveis locais e chamadas de funções (STACK - PILHA)*/
     int var03 { 3 };
-    std::cout << "Chamando Funcao03\n";
-    std::cout << "Endereco var03" << &var03;
-    funcao4;
+    std::cout << "\nChamando Funcao03\n";
+    std::cout << "Endereco var03: " << &var03;
+    funcao4();
 }
 
 void funcao4()
 {
     /*armazenada na região de memória destinnada a variáveis locais e chamadas de funções (STACK - PILHA)*/
     int var04 { 4 };
-    std::cout << "Chamando Funcao04\n";
-    std::cout << "Endereco var04" << &var04;
+    std::cout << "\nChamando Funcao04\n";
+    std::cout << "Endereco var04: " << &var04;
 }
