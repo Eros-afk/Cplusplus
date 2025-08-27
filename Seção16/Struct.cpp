bceee7da06bfe4bb34154a3b668803bff8c2618b
Eros-afk <erosryansimette@alu.ufc.br>
2025-08-27 15:05:26 -0300

    Structs

--- Seção16/Struct.cpp
+++ Seção16/Struct.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 
-class Data{
+struct Data{
     /*Como não foi especificado a struct o acesso será public*/
-    public:
+    static int contador;
+    int id;
     int Dia;
     int Mes;
     int Ano;
-
-    Data(): Dia(22), Mes(7), Ano(2025){}
+    Data(){
+        /*Como contador é estatico ele não vai perder o valor e portanto irá incrementar de um a cada chamada do destrutor*/
+        id = contador++;
+        std::cout << "\nConstrutor de Data(" << id << ")executado" << "\n";
+    }
+    ~Data(){
+        std::cout << "\nDestrutor ~Data(" << id << ")Executado\n";
+    }
 };
 
-int main(){
-    Data Agenda;
-    /*Assim podemos acessar seus atributos*/
-    Agenda.Dia = 22;
-    Agenda.Mes = 7;
-    Agenda.Ano = 2025;
-    
-    std::cout << "Dia: " << Agenda.Dia << "\nMes: " << Agenda.Mes << "\nAno: " << Agenda.Ano;
+int Data::contador = 1;
 
+int main(){
+    {
+    Data Agenda1;
+        {
+            Data Agenda2;
+            /*Agora o Agenda3 está na HEAP e não será destruído ao final do escopo {}, só será destruido se for utilizado a função delete*/
+            Data *Agenda3 = new Data;
+            delete(Agenda3);
+        }
+    Data Agenda4;
+    }
     return 0;
 }
